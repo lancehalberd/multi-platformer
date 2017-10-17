e254f4cebee5cd5f4f6690f1559f5859fb05fb8d
David Evan Krebs <davidevankrebs@davids-mbp.attlocal.net>
2017-10-17 14:06:07 -0500

    Added homing fireball. No animation.

--- app/public/js/localSprites.js
+++ app/public/js/localSprites.js
@@ -1,5 +1,3 @@
-//NEED TO ADD MAX SPEED LIMIT TO SIMPLE SPRITE!
-
 class SimpleSprite {
     constructor(animation, x, y, vx = 0, vy = 0, xScale = 1, yScale = 1) {
         this.animation = animation;
@@ -8,10 +6,12 @@ class SimpleSprite {
         this.vx = vx;
         this.vy = vy;
         this.homing = false;
-        this.target = target;
-        this.maxSpeed = maxSpeed;  //doesn't do anything yet, but will be part of code that limits max speed
+        this.target = {x: 0, y: 0};
+        this.acceleration = 0;
+        this.maxSpeed = 0;
         this.currentFrame = 0;
         this.framesToLive = 200;
+        this.msBetweenFrames = 200;
         // I needed these because the graphic I wanted to use faced up and I need it to face down,
         // which I can get by using yScale = -1
         this.xScale = xScale;
@@ -25,29 +25,71 @@ class SimpleSprite {
 // removing the object
 function updateLocalSprite(localSprite) {
     if (localSprite.homing) { //homing behavior
-        var homerToTargetX = (localSprite.target.x - localSprite.x),  //these two vars are the difference, in x/y values, between the homer's position and its target's position,
-        homerToTargetY = (localSprite.target.y - localSprite.y),        //phrased so that they could be added to the homer's coordinates in order to overlap the target's.
-        normalizedHomerToTargetXRatio,    //these two vars turn that vector into a normalized ratio.
+        var homerToTargetX,  //these two vars are the difference, in x/y values, between the homer's position and its target's position,
+        homerToTargetY,        //phrased so that they could be added to the homer's coordinates in order to overlap the target's.
+        normalizedHomerToTargetXRatio,    //these two vars will turn that vector into a normalized ratio. Actually, sometimes one of the numbers is over 1 right now, but it still works, I *think.*
         normalizedHomerToTargetYRatio,
+        homerX = localSprite.x,
+        homerY = localSprite.y,
+        targetX = localSprite.target.x,
+        targetY = localSprite.target.y,
         dx,
-        dy,
-        accelScale = 1;
-        if (homerToTargeX >= homerToTargetY) {
-            //pick the larger of the X or Y differences in position between homer and mc and use it as the "1" for normalizing both.
-            normalizedHomerToTargeXRatio = homerToTargeX / homerToTargeX; //always results in 1, but presenting it this way  makes it clearer what's going on
-            normalizedHomerToTargetYRatio = homerToTargetY / homerToTargetX;
-        } else {
-            normalizedHomerToTargetYRatio = homerToTargetY / homerToTargetY;
-            normalizedHomerToTargetXRatio = homerToTargetX / homerToTargetY;
+        dy;
+        //finding values for homerToTargetX and homerToTargetY.
+        if ((homerX < targetX && homerX >= 0 && targetX > 0) || (homerX > targetX && homerX > 0 && targetX >= 0) || (homerX > targetX && homerX <= 0 && targetX < 0) || (homerX === targetX)) homerToTargetX = targetX - homerX;
+        if (homerX < targetX && homerX <= 0 && targetX >= 0) homerToTargetX = Math.abs(homerX) + Math.abs(targetX);
+        if (homerX < targetX && homerX < 0 && targetX <= 0) homerToTargetX = Math.abs(homerX) - Math.abs(targetX);
+        if (homerX > targetX && homerX > 0 && targetX < 0) homerToTargetX = -(Math.abs(homerX) + Math.abs(targetX));
+        if ((homerY < targetY && homerY >= 0 && targetY > 0) || (homerY > targetY && homerY > 0 && targetY >= 0) || (homerY > targetY && homerY <= 0 && targetY < 0) || (homerY === targetY)) homerToTargetY = targetY - homerY;
+        if (homerY < targetY && homerY <= 0 && targetY >= 0) homerToTargetY = Math.abs(homerY) + Math.abs(targetY);
+        if (homerY < targetY && homerY < 0 && targetY <= 0) homerToTargetY = Math.abs(homerY) - Math.abs(targetY);
+        if (homerY > targetY && homerY > 0 && targetY < 0) homerToTargetY = -(Math.abs(homerY) + Math.abs(targetY));
+        //making a normalized ratio out of homerToTargetX and homerToTargetY's relationship
+        //NO DIVIDE BY ZERO PROTECTION HERE!!!
+        if (Math.abs(homerToTargetX) > Math.abs(homerToTargetY)) {
+            if (homerToTargetX < 0) {
+                normalizedHomerToTargetXRatio = -(Math.abs(homerToTargetX) / Math.abs(homerToTargetX));
+            } else {
+                normalizedHomerToTargetXRatio = Math.abs(homerToTargetX) / Math.abs(homerToTargetX);
+            }
+            if (homerToTargetY < 0) {
+                normalizedHomerToTargetYRatio = -(Math.abs(homerToTargetY) / Math.abs(homerToTargetX));
+            } else {
+                normalizedHomerToTargetYRatio = Math.abs(homerToTargetY) / Math.abs(homerToTargetX);            
+            }
         }
-        dx = normalizedHomerToTargetXRatio * accelScale;    //scale the normalized ratio for the desired acceleration
-        dy = normalizedHomerToTargetYRatio * accelScale;    
+        if (Math.abs(homerToTargetX) < Math.abs(homerToTargetY)) {
+            if (homerToTargetX < 0) {
+                normalizedHomerToTargetXRatio = -(Math.abs(homerToTargetX) / Math.abs(homerToTargetY));
+            } else {
+                normalizedHomerToTargetXRatio = Math.abs(homerToTargetX) / Math.abs(homerToTargetY);
+            }
+            if (homerToTargetY < 0) {
+                normalizedHomerToTargetYRatio = -(Math.abs(homerToTargetY) / Math.abs(homerToTargetY));
+            } else {
+                normalizedHomerToTargetYRatio = Math.abs(homerToTargetY) / Math.abs(homerToTargetY);            
+            }
+        }
+        dx = normalizedHomerToTargetXRatio * localSprite.acceleration;    //scale the normalized ratio for the desired acceleration
+        dy = normalizedHomerToTargetYRatio * localSprite.acceleration;    
         localSprite.vx += dx;   //add the scaled vector to the homer's velocity
         localSprite.vy += dy;
     }
     localSprite.x += localSprite.vx;
     localSprite.y += localSprite.vy;
-    localSprite.currentFrame = Math.floor(now() / 200) % localSprite.animation.frames.length;
+    //max speed limit:
+    if (localSprite.vx < 0) {
+        localSprite.vx = Math.max(-localSprite.maxSpeed, localSprite.vx);
+    } else {
+        localSprite.vx = Math.min(localSprite.maxSpeed, localSprite.vx);
+    }
+    if (localSprite.vy < 0) {
+        localSprite.vy = Math.max(-localSprite.maxSpeed, localSprite.vy);
+    } else {
+        localSprite.vy = Math.min(localSprite.maxSpeed, localSprite.vy);
+    }
+    //animation stuff. msBetweenFrames sets sprite's animation speed.
+    localSprite.currentFrame = Math.floor(now() / localSprite.msBetweenFrames) % localSprite.animation.frames.length;
     // remove the sprite from the array of local sprites after it has used up all of its frames.
     if (localSprite.framesToLive-- <= 0) {
         // This flag will be used in the update loop to remove this sprite from the list of localSprites.
@@ -77,22 +119,24 @@ function addLocalFallingSpikesSprite() {
     localSprites.push(fallingSpikesSprite);
 }
 
-function addHomingFireball() {
-    var hitbox = rectangle(0, 0, 32, 32);
+function addHomingFireballSprite(xPosition, yPosition, target) {
+    var hitbox = rectangle(0, -16, 32, 32); //the -16 is trying to make the sprites origin at its center, but I don't know if that's what that number does.
     var frames = [
-        $.extend(rectangle(0 * 32, 0 * 32, 32, 32), {image: fireballBImage, hitBox}),
-        $.extend(rectangle(1 * 32, 0 * 32, 32, 32), {image: fireballBImage, hitBox}),
-        $.extend(rectangle(2 * 32, 0 * 32, 32, 32), {image: fireballBImage, hitBox}),
-        $.extend(rectangle(3 * 32, 0 * 32, 32, 32), {image: fireballBImage, hitBox}),
-        $.extend(rectangle(4 * 32, 0 * 32, 32, 32), {image: fireballBImage, hitBox}),
-        /*I asked Chris what the "hitbox" in the above lines meant, and he said:
-         *"That is the same is if I wrote 'hitbox: hitbox'
-It adds a hitbox property to that object and sets it to the local variable with the same name."
-        */
+        $.extend(rectangle(0 * 32, 0 * 32, 32, 32), {image: fireballBImage, hitbox}),
+        //$.extend(rectangle(1 * 32, 0 * 32, 32, 32), {image: fireballBImage, hitbox}),
+        //$.extend(rectangle(2 * 32, 0 * 32, 32, 32), {image: fireballBImage, hitbox}),
+        //$.extend(rectangle(3 * 32, 0 * 32, 32, 32), {image: fireballBImage, hitbox}),
+        //$.extend(rectangle(4 * 32, 0 * 32, 32, 32), {image: fireballBImage, hitbox}),
     ];
-    var homingFireballSprite = new SimpleSprite({frames}, 600, 200, 0, 0, 1, 1);
+    var homingFireballSprite = new SimpleSprite({frames}, xPosition, yPosition, 0, 0, 1.5, 1.5);
     homingFireballSprite.homing = true;
-    homingFireballSprite.target = mainCharacter;
+    homingFireballSprite.target = target;
+    homingFireballSprite.maxSpeed = 3.5;
+    homingFireballSprite.acceleration = 0.8;
     homingFireballSprite.framesToLive = 32767;
+    homingFireballSprite.msBetweenFrames = 50;
     localSprites.push(homingFireballSprite);
 }
+
+
+
--- app/public/js/update.js
+++ app/public/js/update.js
@@ -17,8 +17,12 @@ setInterval(() => {
         updateActor(actor);
     }
     // Example code for adding a new local sprite. We add a new one any time the existing one is removed.
-    if (localSprites.length === 0) {
+    /*if (!localSprites.isArray(fallingSpikesSprite)) {   //this doesn't work. Trying to just check in a fallingSpikesSprite object is in the localSprites array, and if not, add one. Then we can always have one fireball and one set of falling spikes.
         addLocalFallingSpikesSprite();
+    }*/
+    if (localSprites.length === 0) {
+        //addLocalFallingSpikesSprite();
+        addHomingFireballSprite(350, 700, mainCharacter);
     }
     for (var localSprite of localSprites) {
         updateLocalSprite(localSprite);
